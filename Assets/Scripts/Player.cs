d82255e516ff613591c5c130e81138d508584e6b
Mattias Lepp <lepp.mattias@gmail.com>
2018-05-05 13:11:38 +0200

    attention logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,21 +1,58 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
-    public float attention = 0f;
     public float passiveAttentionGain = 0.01f;
-    public float attentionModifier = 0f;
-    public Text attentionText;
-    	
-	void Update () {
-        attention += passiveAttentionGain;
-        if (attention >= 100f)
-        {
-            attention = 0f;
-        }
-        attentionText.text = attention.ToString();
+    public RectTransform attentionMeterPrefab;
+    public RectTransform meterRoot;
 
+    //private Dictionary<EnemyType, float> attentions;
+    //private Dictionary<EnemyType, float> modifiers;
+    //private Dictionary<EnemyType, Text> meters;
+
+    private Dictionary<EnemyType, EnemyAttention> attentions;
+
+    private void Start()
+    {
+        attentions = new Dictionary<EnemyType, EnemyAttention>();
+        foreach (EnemyType type in Enum.GetValues(typeof(EnemyType)))
+        {
+            var meter = Instantiate<RectTransform>(attentionMeterPrefab);
+            meter.SetParent(meterRoot);
+            var texts = meter.GetComponentsInChildren<Text>();
+
+            texts[0].text = type.ToString();
+            var enemyAttention = new EnemyAttention()
+            {
+                Text = texts[1],
+                Attention = 0,
+                Modifier = 0
+            };
+            attentions.Add(type, enemyAttention);
+        }
+    }
+
+    void Update () {
+
+        foreach (EnemyType type in Enum.GetValues(typeof(EnemyType)))
+        {
+            var attention = attentions[type];
+            attention.Attention += passiveAttentionGain + attention.Modifier;
+            attention.Text.text = attention.Attention.ToString();
+
+            if (attention.Attention >= 100)
+            {
+                Debug.Log(string.Format("ENCOUNTER - {0}", type));
+                attention.Attention = 0;
+            }
+        }
 	}
+
+    public void SetAttentionModifier(EnemyType type, float value)
+    {
+        attentions[type].Modifier = value;
+    }
 }
