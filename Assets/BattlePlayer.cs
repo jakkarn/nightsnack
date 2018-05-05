2f06ebb05373f93696785ca08a2349ca14105f68
jakkarn <jakobnorell@gmail.com>
2018-05-05 13:13:49 +0200

    created BattlePlayer script and component

---
+++ Assets/BattlePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattlePlayer : MonoBehaviour {
+
+    public int karisma     = 0;
+    public int friendThreshold = 10;
+    public int enemyThreshold = -10;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
