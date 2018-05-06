687ec35fd99ec271155ba2afd723d00ebb1dd0c7
jakkarn <jakobnorell@gmail.com>
2018-05-06 11:49:53 +0200

    added winning screen when passed body guard

---
+++ Assets/Scripts/Win.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var player = collision.GetComponent<Player>();
+        if (player != null)
+        {
+            SceneManager.LoadScene("Win");
+        }
+
+    }
+}
