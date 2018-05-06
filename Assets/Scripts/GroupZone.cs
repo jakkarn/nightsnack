278c523806040db95b35d8957eabeea5e8acda48
jakkarn <jakobnorell@gmail.com>
2018-05-06 21:59:02 +0200

    added random movement to spotlights

--- Assets/Scripts/GroupZone.cs
+++ Assets/Scripts/GroupZone.cs
@@ -6,6 +6,30 @@ public class GroupZone : MonoBehaviour {
 
     public float attentionGain = 1f;
     public EnemyType enemyType;
+    private Vector2 spotlightDir;
+    private float time;
+    private float movetime;
+    private float x, y;
+
+    private void Start()
+    {
+        x = Random.Range(-0.1f, 0.1f);
+        y = Random.Range(-0.1f, 0.1f);
+        movetime = Random.Range(0, 2);
+        spotlightDir = new Vector2(x, y);
+        time = Time.time;
+    }
+
+    private void Update()
+    {
+        if(Time.time - time > movetime)
+        {
+            spotlightDir = -spotlightDir;
+            time = Time.time;
+        }
+
+        this.GetComponent<Transform>().position += (Vector3)spotlightDir; 
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
