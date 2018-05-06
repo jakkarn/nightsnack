f51ef980adabdfbe7a0bfabc184727b3cab68cf0
jakkarn <jakobnorell@gmail.com>
2018-05-06 22:31:50 +0200

    added key buttons for exiting and restarting, also added scene for key commands

--- Assets/Scripts/exit_script.cs
+++ Assets/Scripts/exit_script.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class intro : MonoBehaviour {
+public class exit_script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
@@ -12,7 +12,9 @@ public class intro : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if(Input.GetKeyDown("space"))
-            SceneManager.LoadScene("Overworld");
+        if (Input.GetKeyDown("escape"))
+            Application.Quit();
+        if(Input.GetKeyDown("p"))
+            SceneManager.LoadScene("Intro");
     }
 }
