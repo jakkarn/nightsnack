4de0264e1b46553130f12a2ab4a0505402ab003d
jakkarn <jakobnorell@gmail.com>
2018-05-06 13:07:45 +0200

    lower amount of friends on end of battle

--- Assets/Scripts/BattlePlayer.cs
+++ Assets/Scripts/BattlePlayer.cs
@@ -24,6 +24,7 @@ public class BattlePlayer : MonoBehaviour {
         if(charisma < enemyThreshold)
         {
             Global.charisma += charisma;
+            Global.numFriendsMade = Global.numFriendsMade >= 1 ? Global.numFriendsMade - 1 : Global.numFriendsMade;
 			SceneManager.LoadScene("Overworld");
 
         }
